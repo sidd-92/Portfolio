a2e101d689543ec989394f4d60ac7a3edeb8be19
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2020-09-27 18:17:10 +0530

    Added Route Animations

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,28 @@
 import App from "next/app";
 import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
 // Styles
 import "../src/assets/main.css";
 class MyApp extends App {
   render() {
-    const { Component, pageProps, store } = this.props;
-    return <Component {...pageProps} />;
+    const { Component, pageProps, store, router } = this.props;
+    return (
+      <motion.div
+        key={router.route}
+        initial="pageInitial"
+        animate="pageAnimate"
+        variants={{
+          pageInitial: {
+            opacity: 0,
+          },
+          pageAnimate: {
+            opacity: 1,
+          },
+        }}
+      >
+        <Component {...pageProps} />
+      </motion.div>
+    );
   }
 }
 
