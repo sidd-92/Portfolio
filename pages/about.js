77395829fdff4db55c659e1554f420f35bb66822
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2020-09-27 09:47:57 +0530

    Link Tag

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+const About = () => (
+  <div>
+    <Link href="/">Home</Link>
+    <h1>About</h1>
+    <div>About Me</div>
+    <img src="/bg.jpg" width="380" height="220" alt="bg" />
+  </div>
+);
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,8 @@
-const Index = () => <div>Home Page</div>;
+import Link from "next/link";
+const Index = () => (
+  <div>
+    <Link href="/about">About</Link>
+    <h1>Home</h1>Home Page
+  </div>
+);
 export default Index;
